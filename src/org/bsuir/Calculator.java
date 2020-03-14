009d310371de33fdbcff4d778a0c0f84cfddfd6b
Yauhen U <zenjo@mail.ru>
2020-03-14 16:43:03 +0300

    Добавлена функция вычитания

--- src/org/bsuir/Calculator.java
+++ src/org/bsuir/Calculator.java
@@ -5,6 +5,10 @@ public static double sum(double first, double second){
 
     return first + second;
 }
+    public static double minus(double first, double second){
+
+        return first - second;
+    }
     public static double multiply(double first, double second){
 
         return first * second;
