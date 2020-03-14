729d4108a12b0c962902a0f75fff40a731bea849
Yauhen U <zenjo@mail.ru>
2020-03-14 16:46:05 +0300

    Добавлена функция умножения

--- src/org/bsuir/Calculator.java
+++ src/org/bsuir/Calculator.java
@@ -5,4 +5,9 @@ public static double sum(double first, double second){
 
     return first + second;
 }
+    public static double multiply(double first, double second){
+
+        return first * second;
+    }
+
 }
