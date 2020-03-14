42dd2e12d78fdc1f340af81a22e980fb2e470f1f
Yauhen U <zenjo@mail.ru>
2020-03-14 16:43:03 +0300

    Добавлена функция вычитания

--- src/org/bsuir/Calculator.java
+++ src/org/bsuir/Calculator.java
@@ -1,8 +1,12 @@
 package org.bsuir;
 
 public class Calculator {
-public static double sum(double first, double second){
+    public static double sum(double first, double second){
 
     return first + second;
 }
+    public static double minus(double first, double second){
+
+        return first - second;
+    }
 }
